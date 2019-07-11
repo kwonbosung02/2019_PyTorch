07e834693bc2858e7294a4acae193930f8a7c61b
bskwon0526@gmail.com <bosungg>
2019-07-12 08:41:49 +0900

    :unicorn:

--- Src/code/code1.py
+++ Src/code/code1.py
@@ -1,1 +1,9 @@
-print("ddkfl")
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
