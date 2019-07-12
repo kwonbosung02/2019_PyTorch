3825180644bef34fe1a0d60164abeaab2e02d4d3
bskwon0526@gmail.com <bosungg>
2019-07-12 09:01:27 +0900

    :unicorn:

--- Src/code/code1.py
+++ Src/code/code1.py
@@ -7,3 +7,5 @@ from torch.utils.data import DataLoader, TensorDataset
 
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
+
+print("ubuntu ssh commit test")
